1b7472efad1a514818c06eb3ca1ac192b2e50204
Ivo Dolenc <ivodolenc@gmail.com>
2025-09-28 16:25:59 +0200

    chore: update output paths

--- bundler.config.ts
+++ bundler.config.ts
@@ -2,15 +2,16 @@ import { defineConfig, resolvePaths, externals } from '@hypernym/bundler'
 
 export default defineConfig({
   entries: [
-    { input: './src/index.mjs' },
-    { dts: './src/types.ts', output: './dist/index.d.mts' },
+    { input: './src/index.js', output: './dist/index.js' },
+    { dts: './src/types.ts', output: './dist/index.d.ts' },
     {
-      input: './src/svelte/index.mjs',
-      externals: [...externals, '../index.mjs'],
+      input: './src/svelte/index.js',
+      output: './dist/svelte/index.js',
+      externals: [...externals, '../index.js'],
       paths: resolvePaths([
-        { find: '../index.mjs', replacement: '../index.mjs' },
+        { find: '../index.js', replacement: '../index.js' },
       ]),
     },
-    { dts: './src/svelte/types.ts', output: './dist/svelte/index.d.mts' },
+    { dts: './src/svelte/types.ts', output: './dist/svelte/index.d.ts' },
   ],
 })
